549a1aa501f6065356e9c00bfa7172ae12486a29
camilufer <camilu.fer21@gmail.com>
2018-03-22 22:33:03 -0300

    editando graficos

---
+++ src/Components/Flujos/Grafico4.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import chart7 from './graficos/chart7.jpg';
+
+
+const Grafico4 = () =>(
+  <div className='Grafico4'>
+    <img src={chart7} alt="chart7" />
+  </div>
+)
+
+export default Grafico4;
---
+++ src/Components/Flujos/Grafico5.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import chart4 from './graficos/chart4.jpg';
+
+
+const Grafico5 = () =>(
+  <div className='Grafico5'>
+    <img src={chart4} alt="chart4" />
+  </div>
+)
+
+export default Grafico5;
