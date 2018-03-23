8a569d0450f76e28acec85f2c33f85d1b63eaed8
MrsPepa <jose.leon1996@gmail.com>
2018-03-23 09:51:39 -0300

    agregando estilo a recibo

--- src/Components/Cobros/Recibo.js
+++ src/Components/Cobros/Recibo.js
@@ -4,40 +4,40 @@ import './../../index.css';
 
 const Recibo = () => (
   <div className='containerRecibido'>
-    <Row className='show-grid'>
+    <Row className='show-grid titleRecibo'>
       <Col xs={12} md={12}>
         <h1>Recibo</h1>
       </Col>
     </Row>
     <Row className='show-grid containerDetail'>
       <Row className='show-grid'>
-        <Col xs={12} md={4}>
-          <h3>Compra realizada: </h3>
+        <Col xs={12} md={6}>
+          <h2>Compra realizada: </h2>
         </Col>
       </Row>
       <Row className='show-grid'>
-        <Col xs={12} md={5}>
-          <p>Libro de Flores </p>
-          <p>Libro de cocina </p>
+        <Col xs={12} md={4} mdOffset={4}>
+          <h3>Libro de Flores </h3>
+          <h3>Libro de cocina </h3>
         </Col>
       </Row>
       <Row className='show-grid'>
-        <Col xs={12} md={4}>
-          <p>IVA: $5.000</p>
+        <Col xs={12} md={4} mdOffset={2}>
+          <h4>IVA: $5.000</h4>
         </Col>
       </Row>
     </Row>
-    <Row className='show-grid'>
+    <Row className='show-grid total'>
        <Col xs={12} md={12}>
         <h2>Total:   $3.000</h2>
       </Col>
     </Row>
     <Row className='show-grid buttons'>
-      <Col xs={12} md={4}>
-        <button>Imprimir</button>
+      <Col xs={12} md={4} mdOffset={2}>
+        <button className='print'>Imprimir</button>
       </Col>
       <Col xs={12} md={4}>
-        <button>Enviar al correo</button>
+        <button className='mail'>Enviar al correo</button>
       </Col>
     </Row>
   </div>
