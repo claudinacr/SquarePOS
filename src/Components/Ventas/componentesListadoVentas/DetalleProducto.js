fa95186418bc8f89b5b1d3f40238740b9da44044
camilabelen <ccornejoss@gmail.com>
2018-03-22 22:37:07 -0300

    maquetado de listado de ventas

---
+++ src/Components/Ventas/componentesListadoVentas/DetalleProducto.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class DetalleProducto extends Component {
+  render() {
+    return (
+      <div className="DetalleProducto">
+        <p>PRODUCTO</p>
+        <p>STOCK</p>
+        <p>precio</p>           
+      </div>
+    );
+  }
+}
+ 
+export default DetalleProducto;
